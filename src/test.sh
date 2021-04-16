f6b9aa449edea72001dd5d76978dddfe0051b952
Hannah Lappe <hannah.lappe0@gmail.com>
2021-04-16 19:00:19 +0200

    formatting changes

--- src/test.sh
+++ src/test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export red="$(tput setaf 1)"
-export green="$(tput setaf 2)"
-export reset="$(tput sgr0)"
+  red="$(tput setaf 1)" ; export red
+green="$(tput setaf 2)" ; export green
+reset="$(tput sgr0)"    ; export reset
 
 failed=""
 
@@ -13,8 +13,8 @@ for input in *.input; do
   variables=$(cat "${name}.variables")
 
   # run ste and timeout if it takes to long
-  actual=$(timeout 1 ../build/ste -p "<#{" -P "}#>" -v "$variables" $input)
-  # actual="testi"
+  actual=$(timeout 1 ../build/ste -p "<#{" -P "}#>" -v "$variables" "$input")
+  # actual="a b c d e f g h i"
   timeout="$?"
 
   diff=$(echo $actual | diff -q "./$expected" -)
@@ -22,12 +22,14 @@ for input in *.input; do
   if   [ "$timeout" == "124" ]; then
     failed="YES"
     printf "%-20s %20s\n" $name "[${red}TIMEOUT${reset}]"
+
   elif [ "$diff"             ]; then
     failed="YES"
     printf "%-20s %20s\n" $name "[${red}FAILED${reset}]"
     echo "$(echo $actual | 
-           diff --color="always" \
-                   "./$expected" -)"
+            diff --color="always" \
+                 "./$expected" -)"
+
   else 
     printf "%-20s %20s\n" $name "[${green}OK${reset}]"
   fi
